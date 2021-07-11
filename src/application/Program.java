1783dc3db9a5801e55de20026a9843959ec77436
Rafael Teixeira Silveira <rateixeira@uolinc.com>
2021-07-11 19:47:05 -0300

    board ok

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import chess.ChessMatch;
+
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Let's get started it");
-		System.out.println("Test");
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+
 	}
 
 }
