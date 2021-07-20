8e244f5488e7cb3bf9a81121f35a106e17be75cc
Rafael Teixeira Silveira <rateixeira@uolinc.com>
2021-07-19 21:56:06 -0300

    add chessException and alter how to put the piece on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -31,17 +31,19 @@ public class ChessMatch  {
         return mat;
 
     }
-    private void initialSetup(){
-        board.placePiece(new Rook(board, Color.BLACK), new Position(0,0));
-        board.placePiece(new Rook(board, Color.BLACK), new Position(0,7));
-        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-        board.placePiece(new Queen(board, Color.BLACK), new Position(0,3));
 
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
-        
-        board.placePiece(new Queen(board, Color.WHITE), new Position(7,3));
-        board.placePiece(new King(board, Color.BLACK), new Position(7,4));
-        board.placePiece(new Rook(board, Color.WHITE), new Position(7,7));
-        board.placePiece(new Rook(board, Color.WHITE), new Position(7,0));
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
+
+    private void initialSetup(){
+        placeNewPiece('a', 1, new Rook(board, Color.BLACK));
+        placeNewPiece('h', 1, new Rook(board, Color.BLACK));
+        placeNewPiece('g', 1, new King(board, Color.BLACK));
+
+        placeNewPiece('b', 7, new King(board, Color.WHITE));
+        placeNewPiece('a', 7, new King(board, Color.WHITE));
+        placeNewPiece('c', 7, new King(board, Color.WHITE));
     }
 }
